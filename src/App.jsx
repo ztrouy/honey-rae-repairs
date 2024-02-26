1e5413d3be894c9821aa67df2a3d31eaf011f640
Zachary Trouy <ztrouy@gmail.com>
2024-02-26 12:50:35 -0600

    Imported and rendered list of employees

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css"
 import { CustomerList } from "./components/customers/CustomerList.jsx"
+import { EmployeeList } from "./components/employees/EmployeeList.jsx"
 import { TicketList } from "./components/tickets/TicketList.jsx"
 
 export const App = () => {
   return <>
     {/* <TicketList/> */}
-    <CustomerList />
+    {/* <CustomerList /> */}
+    <EmployeeList />
   </>
 }
