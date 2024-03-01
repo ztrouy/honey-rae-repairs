55e2274625194f7bf80a42055c69206ea75facf4
Zachary Trouy <34666901+ztrouy@users.noreply.github.com>
2024-03-01 16:37:50 -0600

    Added fetch functions to get a user by email, and create a user

--- src/services/userService.jsx
+++ src/services/userService.jsx
@@ -5,3 +5,20 @@ export const getNonStaffUsers = () => {
 export const getStaffUsers = () => {
     return fetch("http://localhost:8088/users?isStaff=true").then(res => res.json())
 }
+
+export const getUserByEmail = (email) => {
+    return fetch(`http://localhost:8088/users?email=${email}`).then((res) =>
+        res.json()
+    )
+}
+  
+export const createUser = (customer) => {
+    return fetch("http://localhost:8088/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(customer),
+    }).then((res) => res.json())
+}
+  
