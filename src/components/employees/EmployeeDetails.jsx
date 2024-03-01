314b7adfb8b0cc555b3d177543ec8d6b16d91f65
Zachary Trouy <ztrouy@gmail.com>
2024-03-01 14:31:42 -0600

    Wrote rendering function for Employee Detail view

--- src/components/employees/EmployeeDetails.jsx
+++ src/components/employees/EmployeeDetails.jsx
@@ -1,25 +1,26 @@
 import { useParams } from "react-router-dom"
 import { getEmployeeDetails } from "../../services/employeeService.jsx"
 import { useEffect, useState } from "react"
+import "./Employee.css"
 
 export const EmployeeDetails = () => {
     const { employeeId } = useParams()
     
-    // const baseObj = {
-    //     id: 0,
-    //     specialty: "",
-    //     rate: "",
-    //     userId: 0,
-    //     employeeTickets: [],
-    //     user: {
-    //         id: 0,
-    //         fullName: "",
-    //         email: "",
-    //         isStaff: true
-    //     }
-    // }
+    const baseObj = {
+        id: 0,
+        specialty: "",
+        rate: "",
+        userId: 0,
+        employeeTickets: [],
+        user: {
+            id: 0,
+            fullName: "",
+            email: "",
+            isStaff: true
+        }
+    }
 
-    const [employee, setEmployee] = useState({})
+    const [employee, setEmployee] = useState(baseObj)
     
     useEffect(() => {
         getEmployeeDetails(employeeId).then(employee => {
@@ -30,21 +31,21 @@ export const EmployeeDetails = () => {
     console.log(employee)
 
     return (
-        <div className="employee">
-            <div className="employee-header"></div>
+        <section className="employee">
+            <div className="employee-header">{employee.user.fullName}</div>
             <div>
-                <span className="employee-info">Email</span>
-                <span>{employee?.user?.email}</span>
+                <span className="employee-info">Email: </span>
+                <span>{employee.user.email}</span>
             </div>
             <div>
-                <span className="employee-info">Specialty</span>
-                <span>{employee?.specialty}</span>
+                <span className="employee-info">Specialty: </span>
+                <span>{employee.specialty}</span>
             </div>
             <div>
-                <span className="employee-info">Rate</span>
-                <span>{employee?.rate}</span>
+                <span className="employee-info">Rate: </span>
+                <span>{employee.rate}</span>
             </div>
-            <div className="employee-footer">Currently working on {employee?.employeeTickets?.length} tickets</div>
-        </div>
+            <div className="employee-footer">Currently working on {employee.employeeTickets.length} tickets</div>
+        </section>
     )
 }
