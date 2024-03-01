9844db79ff2fbe0bd1128abcc05b52bae6e91a41
Zachary Trouy <34666901+ztrouy@users.noreply.github.com>
2024-03-01 09:32:12 -0600

    Added route to Tickets List view

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
+import { Route, Routes } from "react-router-dom"
 import "./App.css"
 import { CustomerList } from "./components/customers/CustomerList.jsx"
 import { EmployeeList } from "./components/employees/EmployeeList.jsx"
 import { TicketList } from "./components/tickets/TicketList.jsx"
 
 export const App = () => {
-  return <>
-    {/* <TicketList/> */}
-    {/* <CustomerList /> */}
-    <EmployeeList />
-  </>
+  return (
+    <Routes>
+      <Route path="/tickets" element={<TicketList/>} />
+    </Routes>
+  )
 }
