d89a08ecee6eaf1fe879fbd2932ea94dc7131e76
Zachary Trouy <ztrouy@gmail.com>
2024-03-01 14:15:19 -0600

    Wrote rendering function for Customer Detail view

--- src/components/customers/CustomerDetails.jsx
+++ src/components/customers/CustomerDetails.jsx
@@ -1,4 +1,7 @@
+import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
+import { getCustomerDetails } from "../../services/customerService.jsx"
+import "./Customer.css"
 
 export const CustomerDetails = () => {
     // /customer/3
@@ -6,7 +9,44 @@ export const CustomerDetails = () => {
 
     const { customerId } = useParams() // { customerId: 3}
 
+    const baseObj = {
+        id: 0,
+        address: "",
+        phoneNumber: "",
+        userId: 0,
+        user: {
+            id: 0,
+            fullName: "",
+            email: "",
+            isStaff: false
+        }
+    }
+
+    const [customer, setCustomer] = useState(baseObj)
+
+    useEffect(() => {
+        getCustomerDetails(customerId).then(customerArray => {
+            setCustomer(customerArray[0])
+        })
+    }, [])
+
+    console.log(customer)
+
     return (
-        <div>Customer #{customerId}</div>
+        <section className="customer">
+            <div className="customer-header">{customer.user.fullName}</div>
+            <div>
+                <span className="customer-info">Address: </span>
+                <span>{customer.address}</span>
+            </div>
+            <div>
+                <span className="customer-info">Phone Number: </span>
+                <span>{customer.phoneNumber}</span>
+            </div>
+            <div>
+                <span className="customer-info">Email: </span>
+                <span>{customer.user.email}</span>
+            </div>
+        </section>
     )
 }
