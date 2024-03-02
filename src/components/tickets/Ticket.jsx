6236861083c1058aac50acc19b5bf29e3c8f7f93
Zachary Trouy <ztrouy@gmail.com>
2024-03-02 13:55:42 -0600

    Added unused currentUser Prop

--- src/components/tickets/Ticket.jsx
+++ src/components/tickets/Ticket.jsx
@@ -1,13 +1,13 @@
 import { useEffect, useState } from "react"
 
-export const Ticket = ({ ticket, allEmployees }) => {
+export const Ticket = ({ ticket, allEmployees, currentUser }) => {
     const [assignedEmployee, setAssignedEmployee] = useState({})
     
     useEffect(() => {
             const foundEmployee = allEmployees.find(
                 employee => employee.id === ticket.employeeTickets[0]?.employeeId
             )
-            
+
             setAssignedEmployee(foundEmployee)
     }, [ticket, allEmployees])
 
@@ -25,6 +25,13 @@ export const Ticket = ({ ticket, allEmployees }) => {
                 <div>
                     <div className="ticket-info">emergency</div>
                     <div>{ticket.emergency ? "yes" : "no"}</div>
+                </div>
+                <div className="btn-container">
+                    {/* If the logged in user is an employee, and no associated employee ticket */}
+                    {/* Then display a button to claim the ticket */}
+
+                    {/* If the logged in user is the assigned employee for the ticket, and there is no dateCompleted */}
+                    {/* Then display a button to close the ticket */}
                 </div>
             </footer>
         </section>
