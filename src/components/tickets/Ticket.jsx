0fbf77a4752615dbb43a679179c1894bc2844be0
Zachary Trouy <ztrouy@gmail.com>
2024-03-02 13:54:58 -0600

    Added assigned employee name to Tickets, if applicable

--- src/components/tickets/Ticket.jsx
+++ src/components/tickets/Ticket.jsx
@@ -1,13 +1,31 @@
-export const Ticket = ({ ticket }) => {
+import { useEffect, useState } from "react"
+
+export const Ticket = ({ ticket, allEmployees }) => {
+    const [assignedEmployee, setAssignedEmployee] = useState({})
+    
+    useEffect(() => {
+            const foundEmployee = allEmployees.find(
+                employee => employee.id === ticket.employeeTickets[0]?.employeeId
+            )
+            
+            setAssignedEmployee(foundEmployee)
+    }, [ticket, allEmployees])
+
     return (
         <section className="ticket">
             <header className="ticket-info">#{ticket.id}</header>
             <div>{ticket.description}</div>
             <footer>
-            <div>
-                <div className="ticket-info">emergency</div>
-                <div>{ticket.emergency ? "yes" : "no"}</div>
-            </div>
+                <div>
+                    <div className="ticket-info">assignee</div>
+                    <div>
+                        {assignedEmployee ? assignedEmployee.user?.fullName : "None"}
+                    </div>
+                </div>
+                <div>
+                    <div className="ticket-info">emergency</div>
+                    <div>{ticket.emergency ? "yes" : "no"}</div>
+                </div>
             </footer>
         </section>
     )
