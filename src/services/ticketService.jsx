ada267c193d3f10b1bf30f72cee859872b398912
Zachary Trouy <34666901+ztrouy@users.noreply.github.com>
2024-03-02 16:53:25 -0600

    Wrote functions to update Tickets, and to create Employee Tickets

--- src/services/ticketService.jsx
+++ src/services/ticketService.jsx
@@ -1,3 +1,27 @@
 export const getAllTickets = () => {
     return fetch("http://localhost:8088/serviceTickets?_embed=employeeTickets").then(res => res.json())
 }
+
+export const assignTicket = (employeeTicket) => {
+    const postOptions = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(employeeTicket)
+    }
+    
+    return fetch("http://localhost:8088/employeeTickets", postOptions)
+}
+
+export const updateTicket = (ticket) => {
+    const putOptions = {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(ticket)
+    }
+    
+    return fetch(`http://localhost:8088/serviceTickets/${ticket.id}`, putOptions)
+}
