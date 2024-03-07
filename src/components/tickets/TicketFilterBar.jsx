8c134a4405c7903fc71ac2be53563d614957abcf
Zachary Trouy <34666901+ztrouy@users.noreply.github.com>
2024-03-06 23:48:38 -0600

    Added conditional rendering to show buttons based on if the user is a customer or staff

--- src/components/tickets/TicketFilterBar.jsx
+++ src/components/tickets/TicketFilterBar.jsx
@@ -1,29 +1,39 @@
-export const TicketFilterBar = ({ setShowEmergencyOnly, setSearchTerm }) => {
+export const TicketFilterBar = ({ setShowEmergencyOnly, setSearchTerm, setShowOpenOnly ,currentUser }) => {
     
     return (
         <div className="filter-bar">
-            <button 
-                className="filter-btn btn-primary" 
-                onClick={() => {
-                setShowEmergencyOnly(true)
-                }}
-            >
-                Emergency
-            </button>
-            <button 
-                className="filter-btn btn-info" 
-                onClick={() => {
-                setShowEmergencyOnly(false)
-                }}
-            >
-                Show All
-            </button>
-            <input
-                onChange={(event) => {setSearchTerm(event.target.value)}}
-                type="text"
-                placeholder="Search Tickets"
-                className="ticket-search"
-            />
+            {currentUser.isStaff ? (
+                <>
+                    <button 
+                        className="filter-btn btn-primary" 
+                        onClick={() => {
+                        setShowEmergencyOnly(true)
+                        }}
+                    >
+                        Emergency
+                    </button>
+                    <button 
+                        className="filter-btn btn-info" 
+                        onClick={() => {
+                        setShowEmergencyOnly(false)
+                        }}
+                    >
+                        Show All
+                    </button>
+                    <input
+                        onChange={(event) => {setSearchTerm(event.target.value)}}
+                        type="text"
+                        placeholder="Search Tickets"
+                        className="ticket-search"
+                    />
+                </>
+            ) : (
+                <>
+                    <button className="filter-btn btn-primary">Create Ticket</button>
+                    <button className="filter-btn btn-info" onClick={() => setShowOpenOnly(true)}>Open Tickets</button>
+                    <button className="filter-btn btn-secondary" onClick={() => setShowOpenOnly(false)}>All My Tickets</button>
+                </>
+            )}
         </div> 
     )
 }
