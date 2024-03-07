2e0aee0fe1f7468d2ab3c20e6f107c979a4710ba
Zachary Trouy <34666901+ztrouy@users.noreply.github.com>
2024-03-06 22:46:49 -0600

    Removed unused imports

--- src/views/ApplicationViews.jsx
+++ src/views/ApplicationViews.jsx
@@ -1,13 +1,4 @@
-import { Outlet, Route, Routes } from "react-router"
-import { NavBar } from "../components/nav/EmployeeNav.jsx"
-import { Welcome } from "../components/welcome/Welcome.jsx"
-import { TicketList } from "../components/tickets/TicketList.jsx"
-import { CustomerList } from "../components/customers/CustomerList.jsx"
-import { CustomerDetails } from "../components/customers/CustomerDetails.jsx"
-import { EmployeeList } from "../components/employees/EmployeeList.jsx"
-import { EmployeeDetails } from "../components/employees/EmployeeDetails.jsx"
 import { useEffect, useState } from "react"
-import { EmployeeForm } from "../components/forms/EmployeeForm.jsx"
 import { EmployeeViews } from "./EmployeeViews.jsx"
 import { CustomerViews } from "./CustomerViews.jsx"
 
