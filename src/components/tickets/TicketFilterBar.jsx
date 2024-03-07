c7001ee20f9b9228377d752da1a19538b3d9890a
Zachary Trouy <34666901+ztrouy@users.noreply.github.com>
2024-03-07 09:10:59 -0600

    Added link to create new ticket button

--- src/components/tickets/TicketFilterBar.jsx
+++ src/components/tickets/TicketFilterBar.jsx
@@ -1,4 +1,7 @@
+import { useNavigate } from "react-router-dom"
+
 export const TicketFilterBar = ({ setShowEmergencyOnly, setSearchTerm, setShowOpenOnly ,currentUser }) => {
+    const navigate = useNavigate()
     
     return (
         <div className="filter-bar">
@@ -29,7 +32,7 @@ export const TicketFilterBar = ({ setShowEmergencyOnly, setSearchTerm, setShowOp
                 </>
             ) : (
                 <>
-                    <button className="filter-btn btn-primary">Create Ticket</button>
+                    <button className="filter-btn btn-primary" onClick={() => navigate("/tickets/create")}>Create Ticket</button>
                     <button className="filter-btn btn-info" onClick={() => setShowOpenOnly(true)}>Open Tickets</button>
                     <button className="filter-btn btn-secondary" onClick={() => setShowOpenOnly(false)}>All My Tickets</button>
                 </>
