8e5c1c5364a2d5b5a6b023a02c1ca8ae98c619d0
Zachary Trouy <34666901+ztrouy@users.noreply.github.com>
2024-03-07 15:04:17 -0600

    Added fetch to update a customer

--- src/services/customerService.jsx
+++ src/services/customerService.jsx
@@ -1,3 +1,15 @@
 export const getCustomerDetails = (customerId) => {
     return fetch(`http://localhost:8088/customers?userId=${customerId}&_expand=user`).then(res => res.json())
 }
+
+export const updateCustomer = (customer) => {
+    const putOptions = {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(customer)
+    }
+
+    return fetch(`http://localhost:8088/customers/${customer.id}`, putOptions)
+}
